77fae080fec724aba1d61c0f388441c31f6b6f9f
moore-brandon4 <moore_brandon4@columbusstate.edu>
2021-12-09 17:11:08 +0000

    name, ability, and move added

--- project.py
+++ project.py
@@ -37,23 +37,39 @@ def index_post():
     urlBeforeString="https://pokeapi.co/api/v2/pokemon/"+variable
     url=str(urlBeforeString)
     print(url)
-    #response=requests.get(url)
+    
 
     #Gets the url data from the api and prints the json data onto the post html page
     r = requests.get(url)
     pretty_json = json.loads(r.text)
     results4 = pretty_json["forms"][0]["name"]
-    #print("Results for: " + pretty_json.forms.name)
-    #print (json.dumps(pretty_json, sort_keys=True, indent=4))
-    # print(response)
-    #def jprint(obj):
-       #text = json.dumps(obj, sort_keys=True)
-       #print(text)
-    #jprint(response.json())
-    return render_template("index_post.html", pretty_json=pretty_json, results4=results4)
+    if "abilities" in pretty_json:
+        if pretty_json["abilities"]==[]:
+            ability1="none"
+        else:
+          ability1=pretty_json["abilities"][0]["ability"]["name"]
+    else:
+        ability1=pretty_json["abilities"][0]["ability"]["name"]
+
+    if "moves" in pretty_json:
+        if pretty_json["moves"]==[]:
+            move1="none"
+        else:
+          move1=pretty_json["moves"][0]["move"]["name"]  
+    else:
+        move1=pretty_json["moves"][0]["move"]["name"]
+    return render_template("index_post.html", pretty_json=pretty_json, results4=results4, ability1=ability1, move1=move1)
 
 
 if __name__=="__main__":
     project.run(debug=True)              
 
    
+
+#print("Results for: " + pretty_json.forms.name)
+    #print (json.dumps(pretty_json, sort_keys=True, indent=4))
+    # print(response)
+    #def jprint(obj):
+       #text = json.dumps(obj, sort_keys=True)
+       #print(text)
+    #jprint(response.json())
